89822502e17ae7c0efaabbc9b5345c2d5b591f5f
MURILO DE SIQUEIRA VASCONCELOS <msv2079@gmail.com>
2018-05-13 22:21:52 -0300

    Add Files Entities

--- source/TesteSeusConhecimentos/TesteSeusConhecimentos.Entities/FluentSessionFactory.cs
+++ source/TesteSeusConhecimentos/TesteSeusConhecimentos.Entities/FluentSessionFactory.cs
@@ -13,17 +13,21 @@ namespace TesteSeusConhecimentos.Entities
     {
 
         private static ISessionFactory session;
-        private static string connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|TesteSeusConhecimentos.mdf;Integrated Security=True";
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\msv20\Documents\MEGA\projetos\uoledtech-master\source\TesteSeusConhecimentos\TesteSeusConhecimentos.Web\App_Data\TesteSeusConhecimentos.mdf;Integrated Security=True";
 
         public static ISessionFactory criarSession()
         {
-            
+
             if (session != null)
                 return session;
 
             IPersistenceConfigurer configDB = MsSqlConfiguration.MsSql2012.ConnectionString(connectionString);
 
-            var configMap = Fluently.Configure().Database(configDB).Mappings(c => c.FluentMappings.AddFromAssemblyOf<Mapping.UserMap>());
+            var configMap = Fluently.Configure().Database(configDB)
+                .Mappings(c => c.FluentMappings.AddFromAssemblyOf<Mapping.UserMap>())
+                .Mappings(c => c.FluentMappings.AddFromAssemblyOf<Mapping.EnterpriseMap>())
+                .Mappings(c => c.FluentMappings.AddFromAssemblyOf<Mapping.RelationshipMap>());
+
             session = configMap.BuildSessionFactory();
 
             return session;
